58987063f8a7f5a0fce3bea88e0bdc80d5749540
ujumqin <sinegov@gmail.com>
2017-09-07 21:49:53 -0700

    Made progress with finding blank embarkment fields and blank age fields. Replaced with S and mean of age respectively.

--- Titanic/Titanic_Script.R
+++ Titanic/Titanic_Script.R
@@ -1,5 +1,38 @@
 library(dplyr)
-library(xlsx)
+options(max.print = 100000)
 
 titanic <- tbl_df(read.csv("titanic3.csv"))
-read.csv("titanic3.csv")
+
+titanic$embarked[titanic$embarked==""] <- 'S'
+print(titanic$embarked)
+
+titanic$age[is.na(titanic$age)] <- mean(titanic$age, na.rm=TRUE)
+print(titanic$age)
+
+
+
+
+print(titanic)
+
+
+titanic$age[16] == NA
+typeof(titanic$age)
+print(titanic$age)
+mean(titanic$age, na.rm = TRUE)
+
+?mean
+
+if(titanic$age[16] == TRUE) {
+  print("This is true")
+} else {
+  print("This is false")
+}
+
+
+if(is.na(titanic$age[16])) {
+  print("This is na")
+} else {
+  print("Something Else")
+}
+
+
