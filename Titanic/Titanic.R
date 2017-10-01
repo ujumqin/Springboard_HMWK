836c611ecf356a64428d07f2b7fd49fd10344d8a
ujumqin <sinegov@gmail.com>
2017-09-30 22:33:52 -0700

    Fixed comments.

--- Titanic/Titanic.R
+++ Titanic/Titanic.R
@@ -3,13 +3,13 @@
 str(titanic)
 
 # 2 - Use ggplot() for the first instruction
-# Plots a bar graph with the Pclass on the x axis and the count of individuals on the y. The fill color is determined by the gender (sex).
-# Position dodge is used to separate the sexes into two bars as opposed to a stacked bar.
+# Plots a bargraph showing the Pclass on the x axis and the number of Pclass on the y axis Bars are filled in with the gender of these individuals.
+# Dodge is used to split the sexes into two bars, as opposed to a stacked bar.
 ggplot(titanic, aes(x = Pclass, fill = Sex)) + 
   geom_bar(position = "dodge")
 
 # 3 - Plot 2, add facet_grid() layer
-# Facet_grid separates the survivors (1) from the victims (0). Bar is constructed the same as above.
+# Simliar to above, however two graphs are made. The first graph shows those who have perished and the second graph shows survivors.
 ggplot(titanic, aes(x = Pclass, fill = Sex)) + 
   geom_bar(position = "dodge") +
   facet_grid(.~ Survived)
@@ -18,8 +18,8 @@ ggplot(titanic, aes(x = Pclass, fill = Sex)) +
 posn.jd <- position_jitterdodge(0.5, 0, 0.6)
 
 # 5 - Plot 3, but use the position object from instruction 4
-# Plots points of individuals. X represents the Pclass and the y axis represents the age. Color represents the sex. Two graphs are created for survivors and victims. 
-# Size changes the size of the dots and alpha makes the dots transparent.
+# Plots all survivors as a dot with the x-axis showing their P Class, the y-axis showing their age, and the fill color showing their gender. 
+# Size of the dots were enlarged with size and made slightly transparent with alpha.
 ggplot(titanic, aes(x = Pclass, y= Age, color = Sex)) + 
   geom_point(size = 3, alpha = 0.5, position = posn.jd) +
   facet_grid(.~ Survived)
